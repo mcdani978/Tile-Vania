689f4a5ba599fec4f5b1c06a4f54bd6780260bee
mcdani978 <mcdani978@morrisville.edu>
2023-11-22 21:47:55 -0500

    Implement Bullet Behaviour

---
+++ Tile Vania/Assets/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float Flt_bulletSpeed = 20f;
+    Rigidbody2D myRigidbody;
+    PlayerMovementScript player;
+    float Flt_xSpeed;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType<PlayerMovementScript>();
+        Flt_xSpeed = player.transform.localScale.x * Flt_bulletSpeed;
+    }
+
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(Flt_xSpeed, 0f); 
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+}
