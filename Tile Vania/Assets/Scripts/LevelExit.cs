3cc4d9f8305d54b2f109667c81e38ddb9d7b8cd9
mcdani978 <mcdani978@morrisville.edu>
2023-11-27 21:04:18 -0500

    Level Exit Portal

---
+++ Tile Vania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int Int_currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = Int_currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
