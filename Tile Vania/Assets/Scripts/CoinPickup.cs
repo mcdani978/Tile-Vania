2506c308253afe38ff3d2b809b0930dc1dc15440
mcdani978 <mcdani978@morrisville.edu>
2023-12-03 10:24:10 -0500

    Persistent Score & Lives

--- Tile Vania/Assets/Scripts/CoinPickup.cs
+++ Tile Vania/Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] int Int_pointsForCoinPickup = 100;
+
+    bool Bol_wasCollected = false; 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Player")
+        if(other.tag == "Player" && !Bol_wasCollected)
         {
+            Bol_wasCollected = true;
+            FindObjectOfType<GameSession>().AddToScore(Int_pointsForCoinPickup);
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
--- Tile Vania/Assets/Scripts/GameSession.cs
+++ Tile Vania/Assets/Scripts/GameSession.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSession : MonoBehaviour
 { 
 
     [SerializeField] int Int_playerLives = 3;
+    [SerializeField] int Int_score = 0;
+
+    [SerializeField] TextMeshProUGUI livesText; 
+    [SerializeField] TextMeshProUGUI scoreText;
 
     void Awake()
     {
@@ -22,6 +27,12 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        livesText.text = Int_playerLives.ToString();
+        scoreText.text = Int_score.ToString();
+    }
+
     public void ProcessPlayerDeath()
     {
         if (Int_playerLives > 1)
@@ -34,11 +45,18 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void AddToScore(int Int_pointsToAdd)
+    {
+        Int_score += Int_pointsToAdd;
+        scoreText.text = Int_score.ToString();
+    }
+
     void TakeLife()
     {
         Int_playerLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = Int_playerLives.ToString();
     }
 
     private void ResetGameSession()
